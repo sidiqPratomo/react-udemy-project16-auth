d7a27016eb55879fb4d12a156edb64b49b05e6db
pratomosidiq123 <pratomosidiq123@gmail.com>
2023-10-02 15:49:17 +0700

    add useSearchParams and authentication

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,7 +1,7 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
-import NewsletterSignup from './NewsletterSignup';
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
   return (
@@ -9,34 +9,23 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              end
-            >
+            <NavLink to="/" className={({ isActive }) => (isActive ? classes.active : undefined)} end>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/events"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
+            <NavLink to="/events" className={({ isActive }) => (isActive ? classes.active : undefined)}>
               Events
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/newsletter"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
+            <NavLink to="/newsletter" className={({ isActive }) => (isActive ? classes.active : undefined)}>
               Newsletter
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/auth" className={({ isActive }) => (isActive ? classes.active : undefined)}>
+              Authentication
             </NavLink>
           </li>
         </ul>
