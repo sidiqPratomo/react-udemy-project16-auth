cf590586057c8fdd5b534e619e3cbe2bc5bc3f60
pratomosidiq123 <pratomosidiq123@gmail.com>
2023-10-08 20:53:44 +0700

    main navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,12 +11,15 @@ import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 import AuthenticationPage, { action as authAction } from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
+import { tokenLoader } from "./util/auth";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
@@ -61,6 +64,10 @@ const router = createBrowserRouter([
         path: "newsletter",
         element: <NewsletterPage />,
         action: newsletterAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
       },
     ],
   },
